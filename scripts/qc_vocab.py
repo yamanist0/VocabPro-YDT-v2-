687a80e3d67fea7da478af2c1a574be056d0df58
Yamanist <yaman26@proton.me>
2026-08-09 22:20:27 +0300

    structure tweaks

--- scripts/qc_vocab.py
+++ scripts/qc_vocab.py
@@ -10,6 +10,7 @@ def parse_header(line):
     m = email.message.Message()
     m['content-type'] = line
     p = m.get_params()
+    # returns the main content type and a dictionary of parameters
     return m.get_content_type(), dict(p) if p else {}
 cgi_mod.parse_header = parse_header
 sys.modules['cgi'] = cgi_mod
